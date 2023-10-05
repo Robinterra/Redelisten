5bd412e3e0deeae0d27952e25e672be178c72808
Robin DAndrea <robin.dandrea@tu-braunschweig.de>
2023-10-05 15:28:18 +0200

    add main function to cli

--- Redelisten.Cli/Program.cs
+++ Redelisten.Cli/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public static class Program
+{
+    public static int Main(string[] args)
+    {
+        return 0;
+    }
+}
