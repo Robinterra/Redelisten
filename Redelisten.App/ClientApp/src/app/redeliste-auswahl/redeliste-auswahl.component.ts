e92fbc324444128fe7a72ac478115643e7b77414
Michael Schilling <themichaelschilling@gmail.com>
2023-10-24 20:05:30 +0200

    Added routing functionality to list form

--- Redelisten.App/ClientApp/src/app/redeliste-auswahl/redeliste-auswahl.component.ts
+++ Redelisten.App/ClientApp/src/app/redeliste-auswahl/redeliste-auswahl.component.ts
@@ -1,9 +1,8 @@
-import { Component } from '@angular/core';
-import { Teilnehmer } from '../teilnehmer';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from "@angular/router";
 
-import { FormControl } from '@angular/forms';
-import { FormBuilder } from '@angular/forms';
-import { Validators } from '@angular/forms';
+import { forbiddenListNameValidator } from '../shared/forbidden-list-name.directive';
 
 @Component({
   selector: 'app-redeliste-auswahl',
@@ -11,17 +10,34 @@ import { Validators } from '@angular/forms';
   styleUrls: ['./redeliste-auswahl.component.css']
 })
 
-export class RedelisteAuswahlComponent {
+export class RedelisteAuswahlComponent implements OnInit {
 
-  redelisteAuswahlForm = this.fb.group({
-    listName: new FormControl(''),
-    // listName: ['', Validators.required],
-  });
+  redelisteAuswahlForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private router: Router) {}
 
+  ngOnInit(): void {
+    this.redelisteAuswahlForm = new FormGroup({
+      listName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        forbiddenListNameValidator(/bobby/i)
+        // Unbekannter Listenname. Check implementieren
+      ])
+    });
+  }
+
+  /*
   onSubmit() {
-    console.warn(this.redelisteAuswahlForm.value);
+    if (this.redelisteAuswahlForm.valid) {
+      console.log(this.redelisteAuswahlForm.value);
+      // add your form submission logic here
+    }
+  }
+  */
+
+  navigateToRedelisteTeilnahme() {
+    this.router.navigate(['/redeliste-teilnahme']);
   }
 
   updateProfile() {
@@ -29,73 +45,5 @@ export class RedelisteAuswahlComponent {
       listName: 'Nancy',
     });
   }
-
-  /*
-  redelisteAuswahlForm = this.fb.group({
-    listName: ['', Validators.required],
-    teilnehmerName: [''],
-  });
-  */
-
-  // TODO: Work on updating the model
-  // nam = new FormControl('Han');
-
-  /*
-  updateName() {
-    
-  }
-  */
-  // model = new Teilnehmer(11111, 'Hugo');
-  
-  /*
-  joinList() {
-    // this.redelisteAuswahlForm.setValue({listName: 'Redeliste 1'});
-    this.updateName();
-  }
-  */
-  // submitted = false;
-
-  
-
-  /*
-  showFormControls(form: any) {
-    return form && form.controls.name &&
-    form.controls.name.value;
-  }
-  */
-
-  // constructor(private fb: FormBuilder) { }
-
-  // model = new RedelisteTeilnahme('Klaus');
-  /*
-  model = new RedelisteTeilnahme('');
-
-  submitted = false;
-
-  onSubmit() { this.submitted = true; }
-
-  newRedeliste() {
-    this.model = new RedelisteTeilnahme('Peter');
-  }
-  */
-
-  /*
-  btnClick= function () {
-    this.router.navigateByUrl('/user');
-  };
-
-  submitted = false;
-
-
-  onSubmit() {
-    this.submitted = true;
-    
-  }
-
-  showFormControls(form: any) {
-    return form && form.controls.name &&
-    form.controls.name.value;
-  }
-  */
 
 }
