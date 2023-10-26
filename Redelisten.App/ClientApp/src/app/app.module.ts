71deee4803493ced7a117902d2399bb8b9765704
Michael Schilling <themichaelschilling@gmail.com>
2023-10-26 19:16:54 +0200

    Stashing buggy v to revert to compiling v

--- Redelisten.App/ClientApp/src/app/app.module.ts
+++ Redelisten.App/ClientApp/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 // import { HttpClient, HttpHeaders } from '@angular/common/http';
 
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data/in-memory-data.service';
+// import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+// import { InMemoryDataService } from './in-memory-data/in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -21,9 +21,11 @@ import { MessagesComponent } from './messages/messages.component';
     AppRoutingModule,
     RedelisteAuswahlModule,
     HttpClientModule,
+    /*
     HttpClientInMemoryWebApiModule.forRoot(
       InMemoryDataService, { dataEncapsulation: false }
     )
+    */
   ],
   declarations: [
     AppComponent,
