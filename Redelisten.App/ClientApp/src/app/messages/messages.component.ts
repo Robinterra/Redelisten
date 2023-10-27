893e7142348f5fd19cf65bea5f59e23057b44813
Michael Schilling <themichaelschilling@gmail.com>
2023-10-27 18:05:50 +0200

    Attempt to save the project

--- Redelisten.App/ClientApp/src/app/messages/messages.component.ts
+++ Redelisten.App/ClientApp/src/app/messages/messages.component.ts
@@ -4,10 +4,11 @@ import { MessageService } from '../message.service';
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+  styleUrls: ['./messages.component.css'],
 })
+
 export class MessagesComponent {
-
+  
   constructor(public messageService: MessageService) {}
-
+  
 }
