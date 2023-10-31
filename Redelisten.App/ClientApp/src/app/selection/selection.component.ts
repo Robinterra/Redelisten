c01154d412af7ff8be1e9ef6ac9f5d036f472913
Robin DAndrea <robin.dandrea@tu-braunschweig.de>
2023-10-31 19:10:24 +0100

    begin api call

--- Redelisten.App/ClientApp/src/app/selection/selection.component.ts
+++ Redelisten.App/ClientApp/src/app/selection/selection.component.ts
@@ -3,18 +3,24 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from "@angular/router";
 
 import { forbiddenListNameValidator } from '../shared/forbidden-list-name.directive';
+import { Observable } from 'rxjs';
+import { HttpClient } from  '@angular/common/http';
+import { Injectable } from  '@angular/core';
 
 @Component({
   selector: 'app-selection',
   templateUrl: './selection.component.html',
   styleUrls: ['./selection.component.css']
 })
+@Injectable({
+  providedIn:  'root'
+  })
 
 export class SelectionComponent implements OnInit {
 
   selectionForm!: FormGroup;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private http: HttpClient) {}
 
   ngOnInit(): void {
     this.selectionForm = new FormGroup({
@@ -37,7 +43,23 @@ export class SelectionComponent implements OnInit {
   */
 
   navigateToParticipation() {
-    this.router.navigate(['/participation']);
+    alert(this.selectionForm.value.listName);
+    //this.router.navigate(['/participation']);
+    // POST: /api/redeliste/create
+    // JSON: { name: listName }
+    let redeliste = new CreateRedeliste();
+    redeliste.name = this.selectionForm.value.listName;
+    this.createRedeliste(redeliste);
+
+  }
+
+  createRedeliste(redeliste: CreateRedeliste) {
+    // POST: /api/redeliste/create
+    this.http.post('/api/redeliste/create', redeliste).subscribe(
+      (response) => console.log(response),
+      (error) => console.log(error)
+    );
+
   }
 
   updateProfile() {
@@ -46,4 +68,7 @@ export class SelectionComponent implements OnInit {
     });
   }
 
+}
+class CreateRedeliste {
+  name!: string;
 }
